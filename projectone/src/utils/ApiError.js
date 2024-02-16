852489c9876187f1105ca5ada41700f8fc6af9dd
syedalirazazaidi <aaliraza007@hotmail.com>
2024-02-16 23:23:11 +0500

    add error handling

---
+++ projectone/src/utils/ApiError.js
@@ -0,0 +1,28 @@
+class ApiError extends Error {
+    statusCode: any;
+    data: null;
+    success: boolean;
+    errors: never[];
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+
+export {ApiError}
