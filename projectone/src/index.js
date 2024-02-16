49c1b2d48eab4cd98cf2b24c17c58613da2fa7bf
syedalirazazaidi <aaliraza007@hotmail.com>
2024-02-16 21:57:22 +0500

    completed mongodb

--- projectone/src/index.js
+++ projectone/src/index.js
@@ -10,17 +10,13 @@ dotenv.config({
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+      console.log(`⚙️ Server is running at port - : ${process.env.PORT}`);
     });
   })
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
   });
 
-
-
-
-  
 // import express from "express"
 // import mongoose from 'mongoose'
 // import { DB_NAME } from "./constant"
