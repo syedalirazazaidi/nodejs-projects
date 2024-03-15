d7d8e08be5c35a4cbbe7a4aefd2f4e63969e4322
syedalirazazaidi <aaliraza007@hotmail.com>
2024-03-15 21:36:02 +0500

    add basic algo

---
+++ alogorithms/algo.js
@@ -0,0 +1,16 @@
+// reverse a string
+var str=
+"aliraza"
+// const newste=str.split('').reverse().join('')
+// console.log('%calogorithms\algo.js:6 object', 'color: #007acc;', newste);
+var newstr=''
+for (let index = 0; index < str.length; index++) {
+    const element = str[index];
+    newstr=element+newstr
+    // newstr=a 1
+    // newste=l+a 2
+    // newste=+i+l+a 3
+    // and so on
+    
+}
+console.log('%calogorithms\algo.js:13 newstr', 'color: #007acc;', newstr);
--- projectone/src/controllers/user.controller.js
+++ projectone/src/controllers/user.controller.js
@@ -1,7 +1,23 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-   res.status(200).json({
+  const { fullName, email, username, password } = req.body;
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+ 
+  res.status(200).json({
     message: "ok",
   });
 });
